b0ccaf7ad2c0f84810b4645c7e2d44f86ea0d25c
Saumya250620 <78815522+Saumya250620@users.noreply.github.com>
2025-08-10 12:15:58 +0530

    Create script.js

---
+++ Image Search Engine/script.js
@@ -0,0 +1,46 @@
+const searchForm = document.getElementById("search-form");
+const searchBox = document.getElementById("search-box");
+const searchResult = document.getElementById("search-result");
+const showMoreBtn = document.getElementById("show-more-btn");
+const accessKey = "ggv2uM0aqBZscP_e_wTAda3K7tDZe51-Z1ikLdzt-fg";
+
+let keyword = "";
+let page = 1;
+
+async function searchImages(){
+    keyword = searchBox.value.trim();
+
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}&per_page=12`;
+    const response = await fetch(url);
+    const data = await response.json();
+    const results = data.results;
+
+    if(page === 1){
+        searchResult.innerHTML = ""; // Clear old results
+    }
+
+    results.map((result) => {
+        const image = document.createElement("img");
+        image.src = result.urls.small;
+
+        const imageLink = document.createElement("a");
+        imageLink.href = result.links.html;
+        imageLink.target = "_blank";
+
+        imageLink.appendChild(image);
+        searchResult.appendChild(imageLink);
+    });
+
+    showMoreBtn.style.display = "block";
+}
+
+searchForm.addEventListener("submit", (e) =>{
+    e.preventDefault();
+    page = 1;
+    searchImages();
+})
+
+showMoreBtn.addEventListener("click", () => {
+    page++;
+    searchImages();
+});
